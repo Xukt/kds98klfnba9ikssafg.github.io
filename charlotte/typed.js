c905acebe7c56ee851792b0c5118c99b8d958a5e
xkt <118078616+Xukt@users.noreply.github.com>
2023-09-24 00:08:17 +0200

    Update typed.js

--- charlotte/typed.js
+++ charlotte/typed.js
@@ -1,5 +1,5 @@
 new Typed('#typed', {
-    strings: ['xkt on discord','Xkt LLC', 'XUK on spotify','making money while your looking at this','i own Stellar Music Group','kys'], 
+    strings: ['I love my bf <3','charlotteeeeee', 'hihi','chxrlotte._. discord',], 
     typeSpeed: 60,
     delaySpeed: 200,
     showCursor: true,
